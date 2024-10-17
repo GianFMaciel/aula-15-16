15ef402a60622ec3b91f0148b7ff7a9b8c9c352c
Gian F Maciel <gian.f.maciel@gmail.com>
2024-10-16 22:03:51 -0300

    commit

---
+++ exercicio4.js
@@ -0,0 +1,23 @@
+let pokemons = [
+    
+
+]
+let escolhaPokemon = prompt("Digite o número da opção 1-Bulbasauro. 2-Charmander. 3-Squirtle")
+tipoPokemon(pokemons[pergunta])
+
+let tipoPokemon = (pokemon) => {
+    switch (pokemon) {
+        case "Bulbasauro":
+          console.log("Pokemon Bulbasauro Tipo Planta e Veneno")
+          break
+        case "Charmander":
+          console.log("Pokemon Charmander Tipo Fogo")
+          break
+          case "Squirtle":
+          console.log("Pokemon Charmander Tipo Água")
+          break
+        default:
+            console.log("Não encontramos esse pokemon")
+    }
+
+}
